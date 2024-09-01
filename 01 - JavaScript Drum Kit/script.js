eeda28111ab864f40cc46ffcc57895c0cfefceb5
KaChino22 <alex.berchtold90@gmail.com>
2024-09-01 12:20:21 +0200

    Challenge 1 done

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,1 +1,26 @@
-console.log('it works');
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    let keyCode;
+    if (e.key) {
+        keyCode = e.keyCode;
+    } else {
+        keyCode = e.currentTarget.dataset.key;
+    }
+
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+keys.forEach((key) => key.addEventListener('click', playSound));
+window.addEventListener('keydown', playSound);
